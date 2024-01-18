53c271624711481dfd536a3b5b3ea8194a10765b
racksoup <rack.connor1@gmail.com>
2024-01-18 18:26:52 -0500

    customer order tracking

--- client/src/UI/Pages/UserDashboard/Orders/Orders.jsx
+++ client/src/UI/Pages/UserDashboard/Orders/Orders.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Orders.scss';
 import { getCustomerOrders, selectCustomerOrders } from '../../../../Redux/orderSlice';
-import { selectUser } from '../../../../Redux/userSlice';
+import { loadUser, selectUser } from '../../../../Redux/userSlice';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -10,36 +10,60 @@ const Orders = () => {
   const orders = useSelector(selectCustomerOrders);
   const user = useSelector(selectUser);
   const [modal, setModal] = useState(false);
+  const [order, setOrder] = useState(null);
 
   useEffect(() => {
-    dispatch(getCustomerOrders(user.customer_stripe_id));
-    console.log(orders);
+    dispatch(loadUser());
   }, []);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getCustomerOrders(user.customer_stripe_id));
+    }
+  }, [user]);
 
   return (
     <div className='orders'>
-      {modal && <OrdersModal setModal={setModal} />}
+      {modal && <OrdersModal setModal={setModal} order={order} />}
       <h1>Orders and Returns</h1>
       <div className='table'>
-        <div className='order'>
-          <div className='info'>
-            <p>August 23, 2023</p>
-            <p>(3 Items)</p>
-            <p>TOTAL: $47.43</p>
-            <p>SHIP TO: ZeeZee</p>
-            <p>STATUS: Delivered</p>
-          </div>
-          <div className='links'>
-            <p onClick={() => setModal(true)}>Order Details</p>
-            <p>Invoice</p>
-          </div>
-        </div>
+        {orders &&
+          orders.map((order) => {
+            var orderDate = new Date(order.invoice.effective_at * 1000);
+            var options = { year: 'numeric', month: 'long', day: 'numeric' };
+            var formattedDate = new Intl.DateTimeFormat('en-US', options).format(orderDate);
+
+            return (
+              <div className='order'>
+                <div className='info'>
+                  <p>{formattedDate}</p>
+                  <p>{order.invoice.lines.total_count}</p>
+                  <p>{order.invoice.amount_paid}</p>
+                  <p>SHIP TO: Address</p>
+                  <p>STATUS: Delivered</p>
+                </div>
+                <div className='links'>
+                  <p
+                    onClick={() => {
+                      setModal(true);
+                      setOrder(order);
+                    }}
+                  >
+                    Order Details
+                  </p>
+                  <a href={order.invoice.invoice_pdf} target='_blank'>
+                    Invoice PDF
+                  </a>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
 };
 
-const OrdersModal = ({ setModal }) => {
+const OrdersModal = ({ setModal, order }) => {
   const [tab, setTab] = useState('packing list');
 
   return (
@@ -66,11 +90,19 @@ const OrdersModal = ({ setModal }) => {
           <div className='grid'>
             <div className='line1'></div>
             <div className='line2'></div>
-            <div className='top'>
+            <div className='row'>
               <p className='name'>Item Name</p>
               <p className='price'>Price</p>
               <p className='quantity'>Quantity</p>
             </div>
+            {order &&
+              order.invoice.lines.data.map((product) => (
+                <div className='row' style={{ height: '2rem' }}>
+                  <p className='name'>{product.description}</p>
+                  <p className='price'>${product.amount}</p>
+                  <p className='quantity'>{product.quantity}</p>
+                </div>
+              ))}
           </div>
         )}
         {tab == 'invoice details' && <div className='grid'></div>}
