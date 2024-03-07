9c008bf0d0a0bb1f6c7a41fade92b1126c18225f
racksoup <rack.connor1@gmail.com>
2024-03-07 00:16:32 -0500

    fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
-import React, { useEffect } from 'react';
-import './App.scss';
-import Home from './UI/Pages/Home/Home.jsx';
-import AdminLogin from './UI/Pages/AdminLogin/AdminLogin.jsx';
-import UserLogin from './UI/Pages/UserLogin/UserLogin.jsx';
-import AdminDashboard2 from './UI/Pages/AdminDashboard/AdminDashboard2.jsx';
-import UserDashboard from './UI/Pages/UserDashboard/UserDashboard.jsx';
-import CreateUser from './UI/Pages/CreateUser/CreateUser.jsx';
-import Navbar from './UI/Components/Navbar/Navbar.jsx';
-import Footer from './UI/Components/Footer/Footer.jsx';
-import SingleProduct from './UI/Pages/SingleProduct/SingleProduct.jsx';
-import CategoryView from './UI/Pages/CategoryView/CategoryView.jsx';
-import Cart from './UI/Pages/UserDashboard/Cart/Cart.jsx';
-import Checkout from './UI/Pages/Checkout/Checkout.jsx';
-import Categories from './UI/Pages/Categories/Categories.jsx';
-import Orders from './UI/Pages/UserDashboard/Orders/Orders.jsx';
-import Wishlist from './UI/Pages/UserDashboard/Wishlist/Wishlist.jsx';
-import Address from './UI/Pages/UserDashboard/Address/Address.jsx';
-import Security from './UI/Pages/UserDashboard/Security/Security.jsx';
-import Returns from './UI/Pages/UserDashboard/Returns/Returns.jsx';
-import { loadUser } from './Redux/userSlice';
+import React, { useEffect } from "react";
+import "./App.scss";
+import Home from "./UI/Pages/Home/Home.jsx";
+import AdminLogin from "./UI/Pages/AdminLogin/AdminLogin.jsx";
+import UserLogin from "./UI/Pages/UserLogin/UserLogin.jsx";
+import AdminDashboard2 from "./UI/Pages/AdminDashboard/AdminDashboard2.jsx";
+import UserDashboard from "./UI/Pages/UserDashboard/UserDashboard.jsx";
+import CreateUser from "./UI/Pages/CreateUser/CreateUser.jsx";
+import Navbar from "./UI/Components/Navbar/Navbar.jsx";
+import Footer from "./UI/Components/Footer/Footer.jsx";
+import SingleProduct from "./UI/Pages/SingleProduct/SingleProduct.jsx";
+import CategoryView from "./UI/Pages/CategoryView/CategoryView.jsx";
+import Cart from "./UI/Pages/UserDashboard/Cart/Cart.jsx";
+import Checkout from "./UI/Pages/Checkout/Checkout.jsx";
+import Categories from "./UI/Pages/Categories/Categories.jsx";
+import Orders from "./UI/Pages/UserDashboard/Orders/Orders.jsx";
+import Wishlist from "./UI/Pages/UserDashboard/Wishlist/Wishlist.jsx";
+import Address from "./UI/Pages/UserDashboard/Address/Address.jsx";
+import Security from "./UI/Pages/UserDashboard/Security/Security.jsx";
+import Returns from "./UI/Pages/UserDashboard/Returns/Returns.jsx";
+import { loadUser } from "./Redux/userSlice";
 
-import { Route, Routes } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Route, Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -31,27 +31,27 @@ const App = () => {
   }, []);
 
   return (
-    <div className='App-Main'>
-      <div className='App-Background' />
-      <div className='App-Content'>
+    <div className="App-Main">
+      <div className="App-Background" />
+      <div className="App-Content">
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/admin-login' element={<AdminLogin />} />
-          <Route exact path='/user-login' element={<UserLogin />} />
-          <Route exact path='/admin-dashboard' element={<AdminDashboard2 />} />
-          <Route exact path='/user-dashboard' element={<UserDashboard />} />
-          <Route exact path='/create-user' element={<CreateUser />} />
-          <Route exact path='/product' element={<SingleProduct />} />
-          <Route exact path='/category' element={<CategoryView />} />
-          <Route exact path='/categories' element={<Categories />} />
-          <Route exact path='/cart' element={<Cart />} />
-          <Route exact path='/checkout' element={<Checkout />} />
-          <Route exact path='/orders' element={<Orders />} />
-          <Route exact path='/wishlist' element={<Wishlist />} />
-          <Route exact path='/address' element={<Address />} />
-          <Route exact path='/security' element={<Security />} />
-          <Route exact path='/returns' element={<Returns />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/admin-login" element={<AdminLogin />} />
+          <Route exact path="/user-login" element={<UserLogin />} />
+          <Route exact path="/admin-dashboard" element={<AdminDashboard2 />} />
+          <Route exact path="/user-dashboard" element={<UserDashboard />} />
+          <Route exact path="/create-user" element={<CreateUser />} />
+          <Route exact path="/product" element={<SingleProduct />} />
+          <Route exact path="/category" element={<CategoryView />} />
+          <Route exact path="/categories" element={<Categories />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
+          <Route exact path="/wishlist" element={<Wishlist />} />
+          <Route exact path="/address" element={<Address />} />
+          <Route exact path="/security" element={<Security />} />
+          <Route exact path="/returns" element={<Returns />} />
         </Routes>
         <Footer />
       </div>
--- client/src/UI/Pages/Home/Home.jsx
+++ client/src/UI/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
-import './Home.scss';
-import { getHighlightProducts, selectProducts, setCurrProduct } from '../../../Redux/productSlice';
-import Car from '../../../images/car.jpg';
+import React, { useEffect } from "react";
+import "./Home.scss";
+import {
+  getHighlightProducts,
+  selectProducts,
+  setCurrProduct,
+} from "../../../Redux/productSlice";
+import Car from "../../../images/car.jpg";
 
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -15,31 +19,32 @@ const Home = () => {
   }, []);
 
   return (
-    <div className='Home'>
-      <div className='HeaderBox'>
-        <img src={Car} alt='image' className='HeaderImage' />
-        <div className='Gradient' />
-        <h1 className='Title'>GET FIT TODAY!</h1>
-        <div className='Header'>
-          <div className='Btn'>
-            <img src={Car} alt='x' />
-            <p>Weights</p>
+    <div className="home">
+      <div className="headerImage">
+        <img src={Car} alt="image" />
+      </div>
+      <div className="header">
+        <h1 className="title">GET FIT TODAY!</h1>
+        <div className="store-categories">
+          <div className="category-widget">
+            <h2>Weights</h2>
+            <img src={Car} alt="" />
           </div>
-          <div className='Btn'>
-            <img src={Car} alt='x' />
-            <p>Gym Systems</p>
+          <div className="category-widget">
+            <h2>Gym Systems</h2>
+            <img src={Car} alt="" />
           </div>
-          <div className='Btn'>
-            <img src={Car} alt='x' />
-            <p>Plate Loaded</p>
+          <div className="category-widget">
+            <h2>Plate Loaded</h2>
+            <img src={Car} alt="" />
           </div>
-          <div className='Btn'>
-            <img src={Car} alt='x' />
-            <p>Accessories</p>
+          <div className="category-widget">
+            <h2>Accessories</h2>
+            <img src={Car} alt="" />
           </div>
         </div>
       </div>
-      <div className='Section1'>
+      <div className="section1">
         {products &&
           products.map((x, i) => {
             if (i < 6)
@@ -48,10 +53,10 @@ const Home = () => {
                   {x.image_filenames.map((c, j) => {
                     if (c.main) {
                       return (
-                        <Link key={i} className='product' to='/product'>
+                        <Link key={i} className="product" to="/product">
                           <img
                             src={`/api/product/image/${c.filename}`}
-                            alt='image'
+                            alt="image"
                             onClick={() => dispatch(setCurrProduct(x))}
                           />
                         </Link>
@@ -62,15 +67,15 @@ const Home = () => {
               );
           })}
       </div>
-      <div className='Section2'>
-        <img className='promo' src={Car} alt='' />
-        <div className='category'>
+      <div className="section2">
+        <img className="promo" src={Car} alt="" />
+        <div className="category-widget">
           <h2>Bicycles</h2>
-          <img src={Car} alt='' />
+          <img src={Car} alt="" />
         </div>
-        <div className='category'>
+        <div className="category-widget">
           <h2>Bicycles</h2>
-          <img src={Car} alt='' />
+          <img src={Car} alt="" />
         </div>
       </div>
     </div>
