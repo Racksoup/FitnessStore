987cc8cd570d4c6aa27da56123006ac81ccdfbe5
racksoup <rack.connor1@gmail.com>
2024-03-17 14:39:23 -0400

    user-orders mob

--- client/src/UI/Pages/UserDashboard/Orders/Orders.jsx
+++ client/src/UI/Pages/UserDashboard/Orders/Orders.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './Orders.scss';
+import React, { useState, useEffect } from "react";
+import "./Orders.scss";
 import {
   getCustomerOrders,
   selectCustomerOrders,
   setCustomerOrder,
-} from '../../../../Redux/orderSlice';
-import { loadUser, selectUser } from '../../../../Redux/userSlice';
+} from "../../../../Redux/orderSlice";
+import { loadUser, selectUser } from "../../../../Redux/userSlice";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
   const dispatch = useDispatch();
@@ -16,6 +16,18 @@ const Orders = () => {
   const user = useSelector(selectUser);
   const [modal, setModal] = useState(false);
   const [order, setOrder] = useState(null);
+  const [windowWidth, setWindowWidth] = useState(1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     dispatch(loadUser());
@@ -28,38 +40,69 @@ const Orders = () => {
   }, [user]);
 
   return (
-    <div className='orders'>
+    <div className="orders">
       {modal && <OrdersModal setModal={setModal} order={order} />}
       <h1>Orders and Returns</h1>
-      <div className='table'>
+      <div className="table">
         {orders &&
           orders.map((order) => {
             var orderDate = new Date(order.invoice.effective_at * 1000);
-            var options = { year: 'numeric', month: 'long', day: 'numeric' };
-            var formattedDate = new Intl.DateTimeFormat('en-US', options).format(orderDate);
+            let options = { year: "numeric", month: "long", day: "numeric" };
+            if (windowWidth <= 768) {
+              options = { year: "numeric", month: "2-digit", day: "2-digit" };
+            }
+            var formattedDate = new Intl.DateTimeFormat(
+              "en-US",
+              options
+            ).format(orderDate);
 
             return (
-              <div className='order'>
-                <div className='info'>
+              <div className="order">
+                <div className="info">
                   <p>{formattedDate}</p>
                   <p>{order.invoice.lines.total_count}</p>
                   <p>{order.invoice.amount_paid}</p>
-                  <p>SHIP TO: Address</p>
-                  <p>STATUS: Delivered</p>
+                  {windowWidth > 768 ? (
+                    <>
+                      <p>SHIP TO: Address</p>
+                      <p>STATUS: Delivered</p>
+                    </>
+                  ) : (
+                    <>
+                      <p>Address</p>
+                      <p className="hide">Delivered</p>
+                    </>
+                  )}
                 </div>
-                <div className='links'>
-                  <p
-                    onClick={() => {
-                      setModal(true);
-                      setOrder(order);
-                    }}
-                  >
-                    Order Details
-                  </p>
-                  <a href={order.invoice.invoice_pdf} target='_blank'>
-                    Invoice PDF
-                  </a>
-                </div>
+                {windowWidth > 768 ? (
+                  <div className="links">
+                    <p
+                      onClick={() => {
+                        setModal(true);
+                        setOrder(order);
+                      }}
+                    >
+                      Order Details
+                    </p>
+                    <a href={order.invoice.invoice_pdf} target="_blank">
+                      Invoice PDF
+                    </a>
+                  </div>
+                ) : (
+                  <div className="links">
+                    <p
+                      onClick={() => {
+                        setModal(true);
+                        setOrder(order);
+                      }}
+                    >
+                      Details
+                    </p>
+                    <a href={order.invoice.invoice_pdf} target="_blank">
+                      Invoice
+                    </a>
+                  </div>
+                )}
               </div>
             );
           })}
@@ -70,50 +113,52 @@ const Orders = () => {
 
 const OrdersModal = ({ setModal, order }) => {
   const dispatch = useDispatch();
-  const [tab, setTab] = useState('packing list');
+  const [tab, setTab] = useState("packing list");
 
   return (
-    <div className='ordersModal' onClick={() => setModal(false)}>
-      <div className='content' onClick={(e) => e.stopPropagation()}>
-        <div className='buttons'>
-          <div className='tabs'>
+    <div className="ordersModal" onClick={() => setModal(false)}>
+      <div className="content" onClick={(e) => e.stopPropagation()}>
+        <div className="buttons">
+          <div className="tabs">
             <button
-              onClick={() => setTab('packing list')}
-              className={`${tab == 'packing list' && 'blue'}`}
+              onClick={() => setTab("packing list")}
+              className={`${tab == "packing list" && "blue"}`}
             >
               Packing List
             </button>
             <button
-              onClick={() => setTab('invoice details')}
-              className={`${tab == 'invoice details' && 'blue'}`}
+              onClick={() => setTab("invoice details")}
+              className={`${tab == "invoice details" && "blue"}`}
             >
               Invoice Details
             </button>
           </div>
-          <Link to='/returns' className='return'>
-            <p onClick={() => dispatch(setCustomerOrder(order))}>Return Items</p>
+          <Link to="/returns" className="return">
+            <p onClick={() => dispatch(setCustomerOrder(order))}>
+              Return Items
+            </p>
           </Link>
         </div>
-        {tab == 'packing list' && (
-          <div className='grid'>
-            <div className='line1'></div>
-            <div className='line2'></div>
-            <div className='row'>
-              <p className='name'>Item Name</p>
-              <p className='price'>Price</p>
-              <p className='quantity'>Quantity</p>
+        {tab == "packing list" && (
+          <div className="grid">
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="row">
+              <p className="name">Item Name</p>
+              <p className="price">Price</p>
+              <p className="quantity">Quantity</p>
             </div>
             {order &&
               order.invoice.lines.data.map((product) => (
-                <div className='row' style={{ height: '2rem' }}>
-                  <p className='name'>{product.description}</p>
-                  <p className='price'>${product.amount}</p>
-                  <p className='quantity'>{product.quantity}</p>
+                <div className="row" style={{ height: "2rem" }}>
+                  <p className="name">{product.description}</p>
+                  <p className="price">${product.amount}</p>
+                  <p className="quantity">{product.quantity}</p>
                 </div>
               ))}
           </div>
         )}
-        {tab == 'invoice details' && <div className='grid'></div>}
+        {tab == "invoice details" && <div className="grid"></div>}
       </div>
     </div>
   );
